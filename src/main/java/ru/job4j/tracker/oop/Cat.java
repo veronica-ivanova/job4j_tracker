ef1171ebe02156b594c2254588bb9a5f3c5f181d
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-04-28 14:12:59 +0300

    1.5. Поля объекта

---
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Ryuzshik");
+        gav.show();
+        System.out.println("There are black's food");
+        Cat black = new Cat();
+        black.giveNick("Barsik");
+        black.eat("fish");
+        black.show();
+    }
+}
