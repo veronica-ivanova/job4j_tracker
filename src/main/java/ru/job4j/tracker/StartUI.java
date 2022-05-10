e4e57adfd7f39d72cb6e0a8b3b38b6ab67bf36ea
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-05-10 15:05:08 +0300

    9. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,10 +5,14 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item("item1");
+        Item item = new Item("item");
         LocalDateTime dateTime = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String dateTimeFormat = dateTime.format(formatter);
         System.out.println(dateTimeFormat);
+
+        Item item1 = new Item(1, "item1");
+        System.out.println(item1);
+
     }
 }
