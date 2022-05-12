45ea88dd2f1b38cf0eb48bcc1656d3c8580b4d9e
veronica-ivanova <veronica.ivanova27@gmail.com>
2022-05-12 16:51:45 +0300

    3. Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int x) {
+
+    }
+
+    @Override
+    public int gas(int x) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    public void go();
+
+    public void passengers(int x);
+
+    public int gas(int x);
+}
